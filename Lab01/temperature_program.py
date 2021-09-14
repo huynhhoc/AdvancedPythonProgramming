2fd49d16171a0e2bb63280f3b24f3897d71b1a2e
huynhhoc <huynhhoc@gmail.com>
2021-09-14 14:01:16 +0200

    update temperature_program

--- Lab01/temperature_program.py
+++ Lab01/temperature_program.py
@@ -1,6 +1,8 @@
 def convert_to_celsius(fahrenheit: float) -> float:
     return (fahrenheit - 32.0)*5.0/9.0
+def above_freezing(celsius):
     return celsius > 0
+# dong tu 7 - 12: khong phai la main cua baking.py
 if __name__ == '__main__':
     fahrenheit = float(input('Enter the temperature in degrees Fahrenheit: '))
     celsius = convert_to_celsius(fahrenheit)
