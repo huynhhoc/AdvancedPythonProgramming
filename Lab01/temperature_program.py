ab917ef7bbab4c223fc5eecdb1b6dc943b1a1539
huynhhoc <huynhhoc@gmail.com>
2021-09-27 15:16:49 +0200

    update temp

--- Lab01/temperature_program.py
+++ Lab01/temperature_program.py
@@ -1,7 +1,7 @@
 def convert_to_celsius(fahrenheit: float) -> float:
-    """Return the number of Celsius degrees equivalent to fahrenheit degrees.
+    """Return the number of celsius degrees equivalent to fahrenheit degrees
     >>> convert_to_celsius(75)
-    23.88888888889
+    23.88888888888889
     """
     return (fahrenheit - 32.0)*5.0/9.0
 def above_freezing(celsius: float) -> bool:
